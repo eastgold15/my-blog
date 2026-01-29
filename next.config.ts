ac8d0339b643cc09ecbe650d934cfe4fd0d6550c
PoRi <1960825664@qq.com>
2026-01-29 23:18:52 +0800

    安装mdx

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
+import createMDX from '@next/mdx'
 // 只有在 GitHub Actions 环境下才设置前缀，Vercel 不需要
 const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 const nextConfig: NextConfig = {
@@ -13,6 +13,16 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   basePath: isGitHubPages ? '/my-blog' : '',
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+  // Add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
+
