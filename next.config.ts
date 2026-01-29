3592cd4c48be85f525390227ae0dc6f123506ae9
PoRi <1960825664@qq.com>
2026-01-29 22:14:23 +0800

    github action

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // 关键：生成静态 HTML/CSS/JS
+  output: "export",
+  images: {
+    // GitHub Pages 不支持 Next.js 默认的图片优化
+    // 禁用 Next.js 默认的图片优化
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
