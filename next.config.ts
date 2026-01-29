f505061d821509133a4cf275fe5f45c01ac317be
PoRi <1960825664@qq.com>
2026-01-29 22:20:00 +0800

    ``` feat(next-config): 添加GitHub Pages部署配置支持
    - 新增环境变量判断逻辑，区分生产环境和GitHub Actions环境
    - 配置basePath用于GitHub Pages的路径前缀设置
    - 在GitHub Actions环境下设置base路径为'/my-blog'
    - 保持Vercel部署不受影响
    ```

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
+// 只有在 GitHub Actions 环境下才设置前缀，Vercel 不需要
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
@@ -9,7 +11,8 @@ const nextConfig: NextConfig = {
     // GitHub Pages 不支持 Next.js 默认的图片优化
     // 禁用 Next.js 默认的图片优化
     unoptimized: true,
-  }
+  },
+  basePath: isGitHubPages ? '/my-blog' : '',
 };
 
 export default nextConfig;
